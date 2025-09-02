5a919b7354c75a7f40cc148cd0bedf51c979071e
renz0826 <brenzbedonia@gmail.com>
2025-09-02 08:58:17 +0800

    feat: warning message for required fields

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ function solve() {
   const employeeName = document.getElementById("name").value;
   const hours = parseFloat(document.getElementById("hours").value) || 0;
   const rate = parseFloat(document.getElementById("rate").value) || 0;
+  const warning = document.getElementById("warning");
+
+  // Intialize warning content to blank
+  warning.textContent = "";
+
+  if (!employeeName || isNaN(hours) || isNaN(rate)) {
+    warning.textContent = "⚠️ Please fill out all the fields to proceed.";
+    return;
+  }
+
+  if (hours < 0 || rate < 0) {
+    warning.textContent = "⚠️ Hours and rate must be a non-negative.";
+    return;
+  }
 
   // Salary computation
   const salary = hours * rate;
 
   // Update output fields
-  let outEmployee = (document.getElementById("out-name").textContent =
-    employeeName);
-  let outHours = (document.getElementById("out-hours").textContent = hours);
-  let outRate = (document.getElementById("out-rate").textContent =
-    rate.toFixed(2));
-  let outSalary = (document.getElementById("out-salary").textContent =
-    salary.toFixed(2));
+  document.getElementById("out-name").textContent = employeeName;
+  document.getElementById("out-hours").textContent = hours;
+  document.getElementById("out-rate").textContent = rate.toFixed(2);
+  document.getElementById("out-salary").textContent = salary.toFixed(2);
 }
 
 // Attach event listener
