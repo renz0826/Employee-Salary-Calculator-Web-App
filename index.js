3bef6b5dc3b0ff4e833cafad15accc7150fde304
renz0826 <brenzbedonia@gmail.com>
2025-09-02 13:40:23 +0800

    feat: removed warning

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ function solve() {
   const employeeName = document.getElementById("name").value;
   const hours = parseFloat(document.getElementById("hours").value) || 0;
   const rate = parseFloat(document.getElementById("rate").value) || 0;
-  const warning = document.getElementById("warning");
-
-  // Intialize warning content to blank
-  warning.textContent = "";
-
-  if (!employeeName || isNaN(hours) || isNaN(rate)) {
-    warning.textContent = "⚠️ Please fill out all the fields to proceed.";
-    return;
-  }
-
-  if (hours < 0 || rate < 0) {
-    warning.textContent = "⚠️ Hours and rate must be a non-negative.";
-    return;
-  }
 
   // Salary computation
   const salary = hours * rate;
@@ -26,8 +12,10 @@ function solve() {
   document.getElementById("out-name").textContent = employeeName;
   document.getElementById("out-hours").textContent = hours;
   document.getElementById("out-rate").textContent = rate.toFixed(2);
-  document.getElementById("out-salary").textContent = salary.toFixed(2);
+  document.getElementById("out-salary").textContent = "₱ " + salary.toFixed(2);
 }
 
-// Attach event listener
-document.getElementById("solve").addEventListener("click", solve);
+document.querySelector(".SalaryForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  solve();
+});
